76c4ae40b7cf3427edf6de6c041ed8536f67b2d9
Amrr Bakry <amrrbakry17@gmail.com>
2017-02-07 21:56:36 +0200

    add correct user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,15 @@
 class UsersController < ApplicationController
+	before_action :correct_user
+
   def show
   	@user = User.find(params[:id])
   end
+
+  private
+
+  	# current user only views his own profile
+  	def correct_user
+  		@user = User.find(params[:id])
+  		redirect_to(root_url) unless current_user == @user
+  	end
 end
