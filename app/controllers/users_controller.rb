f44c8a9fe4768b2433d499fcbc4d6d1244fc7268
Amrr Bakry <amrrbakry17@gmail.com>
2017-02-07 22:06:27 +0200

    show user name in user view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,6 @@
 class UsersController < ApplicationController
-
   before_action :authenticate_user!
-	before_action :correct_user
 
   def show
-  	@user = User.find(params[:id])
   end
-
-  private
-
-  	# current user only views his own profile
-  	def correct_user
-  		redirect_to(root_url) unless current_user == @user
-  	end
 end
