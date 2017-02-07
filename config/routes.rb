e9141e4239e83a929067a038a9d1a08b84b70f89
Amrr Bakry <amrrbakry17@gmail.com>
2017-02-07 16:38:59 +0200

    add name field to registrations

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users, controllers: { sessions: 'users/sessions' ,
+  								  registrations: 'users/registrations',
+  								  confirmations: 'users/confirmations',
+  								  shared: 'users/shared'
+  								}
   root to: "static_pages#home"
 end
