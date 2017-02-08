0a10c1a5db32a88e1a07a6391a982d90362af949
Amrr Bakry <amrrbakry17@gmail.com>
2017-02-09 01:22:30 +0200

    change categories views

---
+++ test/controllers/categories_controller_test.rb
@@ -0,0 +1,4 @@
+require 'test_helper'
+
+class CategoriesControllerTest < ActionDispatch::IntegrationTest
+end
--- test/integration/adding_item_to_category_test.rb
+++ test/integration/adding_item_to_category_test.rb
@@ -1,9 +1,4 @@
 require 'test_helper'
 
 class AddingItemToCategoryTest < ActionDispatch::IntegrationTest
-	include Devise::Test::IntegrationHelpers
-
-	def setup
-		@user = User.first
-	end
 end
