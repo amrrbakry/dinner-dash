b44eace837b48b25b7394d0cab978c415383763b
Amrr Bakry <amrrbakry17@gmail.com>
2017-04-17 16:16:22 +0200

    fix rubocop offenses test/models.rb

--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -1,38 +1,38 @@
 require 'test_helper'
 
 class CategoryTest < ActiveSupport::TestCase
-	def setup 
-		@category = Category.create(title: "Fruits", description: "Fresh fruits")
-	end
+  def setup
+    @category = Category.create(title: "Fruits", description: "Fresh fruits")
+  end
 
-	test "category is valid" do
-		assert @category.valid?
-	end
+  test "category is valid" do
+    assert @category.valid?
+  end
 
-	test "title is present" do
-		@category.title = nil
-		assert_not @category.valid?
-	end
+  test "title is present" do
+    @category.title = nil
+    assert_not @category.valid?
+  end
 
-	test "description is present" do
-		@category.description = nil
-		assert_not @category.valid?
-	end
+  test "description is present" do
+    @category.description = nil
+    assert_not @category.valid?
+  end
 
-	test "title is shorter than 150 chars" do
-		@category.title = "a" * 151
-		assert_not @category.valid?
-	end
+  test "title is shorter than 150 chars" do
+    @category.title = "a" * 151
+    assert_not @category.valid?
+  end
 
-	test "title is longer than 2 chars" do
-		@category.title = "a"
-		assert_not @category.valid?
-	end
+  test "title is longer than 2 chars" do
+    @category.title = "a"
+    assert_not @category.valid?
+  end
 
-	test "description is within 3..400 chars" do
-		@category.description = "a" * 401
-		assert_not @category.valid?
-		@category.description = "aa"
-		assert_not @category.valid?
-	end
+  test "description is within 3..400 chars" do
+    @category.description = "a" * 401
+    assert_not @category.valid?
+    @category.description = "aa"
+    assert_not @category.valid?
+  end
 end
--- test/models/item_test.rb
+++ test/models/item_test.rb
@@ -1,51 +1,49 @@
 require 'test_helper'
 
 class ItemTest < ActiveSupport::TestCase
-	def setup
-		@category = categories(:one)
-		@item = @category.items.create(title: "Milk", description: "a catron of milk", price: 12.00)
-	end
+  def setup
+    @category = categories(:one)
+    @item = @category.items.create(title: "Milk", description: "a catron of milk", price: 12.00)
+  end
 
-	test "item is valid" do
-		assert @item.valid?
-	end
+  test "item is valid" do
+    assert @item.valid?
+  end
 
-	test "title is presnet" do
-		@item.title = nil
-		assert_not @item.valid?
-	end
+  test "title is presnet" do
+    @item.title = nil
+    assert_not @item.valid?
+  end
 
-	test "title is unique" do
-		item2 = @item.dup
-		item2.save
-		assert_includes item2.errors, :title
-		assert_includes item2.errors[:title], "Title has to be unique"
-	end
+  test "title is unique" do
+    item2 = @item.dup
+    item2.save
+    assert_includes item2.errors, :title
+    assert_includes item2.errors[:title], "Title has to be unique"
+  end
 
-	test "description is presnet" do
-		@item.description = nil
-		assert_not @item.valid?
-	end
+  test "description is presnet" do
+    @item.description = nil
+    assert_not @item.valid?
+  end
 
-	test "price is present" do
-		@item.price = nil
-		assert_not @item.valid?
-	end
+  test "price is present" do
+    @item.price = nil
+    assert_not @item.valid?
+  end
 
-	test "price is greater than zero" do
-		@item.price = -2.00
-		assert_not @item.valid?
-		@item.price < 0.00
-		assert_not @item.valid?
-	end
+  test "price is greater than zero" do
+    @item.price = -2.00
+    assert_not @item.valid?
+  end
 
-	test "default picture is used if no picture is provided" do
-		item = Item.new(title: "t", description: "d", price: 5.00)
-		item.save
-		assert_equal item.picture.url, "default_item_pic.jpg"
-	end
+  test "default picture is used if no picture is provided" do
+    item = Item.new(title: "t", description: "d", price: 5.00)
+    item.save
+    assert_equal item.picture.url, "default_item_pic.jpg"
+  end
 
-	test "item belong to at least one category" do
-		assert_not @item.categories.empty?
-	end
+  test "item belong to at least one category" do
+    assert_not @item.categories.empty?
+  end
 end
