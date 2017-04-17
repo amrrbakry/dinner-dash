20ee403fb56ebd85125ddfc93b6d47cdc02a867e
Amrr Bakry <amrrbakry17@gmail.com>
2017-04-17 16:08:08 +0200

    fix rubocop offenses in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,29 +1,23 @@
 Rails.application.routes.draw do
-
-  devise_for :users, controllers: { sessions: 'users/sessions' ,
-  								  registrations: 'users/registrations',
-  								  confirmations: 'users/confirmations',
-  								  shared: 'users/shared'
-  								}
- 	resources :users, only: :show
+  devise_for :users, controllers: { sessions: 'users/sessions',
+                                    registrations: 'users/registrations',
+                                    confirmations: 'users/confirmations',
+                                    shared: 'users/shared' }
+  resources :users, only: :show
 
   devise_scope :user do
     get 'sign_in', to: 'devise/sessions#new'
   end
-  
   root to: "items#index"
-  
-  namespace :admin do 
-    resources :categories do 
-      resources :items 
+  namespace :admin do
+    resources :categories do
+      resources :items
     end
     resources :items
   end
-  
-  resources :items, only: [:index, :show]
+  resources :items, only: %i[index show]
 
-  resources :categories, only: [:index, :show] do
-    resources :items,    only: [:index, :show]
+  resources :categories, only: %i[index show] do
+    resources :items, only: %i[index show]
   end
-
 end
