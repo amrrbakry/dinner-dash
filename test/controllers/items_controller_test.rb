56568f2dc96ed2cd4da9ea5de4ab88264178543a
Amrr Bakry <amrrbakry17@gmail.com>
2017-04-17 16:17:13 +0200

    fix rubocop offenses test/controllers

--- test/controllers/items_controller_test.rb
+++ test/controllers/items_controller_test.rb
@@ -1,10 +1,9 @@
 require 'test_helper'
 
 class ItemsControllerTest < ActionDispatch::IntegrationTest
-
-	test "default picture is used if no picture is provided" do
-		item = Item.new(title: "t", description: "d", price: 5.00)
-		item.save
-		assert_equal item.picture.url, "default_item_pic.jpg"
-	end
+  test "default picture is used if no picture is provided" do
+    item = Item.new(title: "t", description: "d", price: 5.00)
+    item.save
+    assert_equal item.picture.url, "default_item_pic.jpg"
+  end
 end
