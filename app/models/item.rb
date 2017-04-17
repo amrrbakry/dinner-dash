7a7e33ab2ccabb5f8bacc6d005fa536915da1652
Amrr Bakry <amrrbakry17@gmail.com>
2017-04-17 15:22:06 +0200

    fix rubocop offenses in all models

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,10 +1,10 @@
 class Item < ApplicationRecord
-	validates :title, 			presence: true, uniqueness: { message: "%{attribute} has to be unique" }
-	validates :description, presence: true
-	validates :price, 			presence: true, numericality: { greater_than: 0 }
+  validates :title, presence: true, uniqueness: { message: "%{attribute} has to be unique" }
+  validates :description, presence: true
+  validates :price,  presence: true, numericality: { greater_than: 0 }
 
   has_attached_file :picture, styles: { medium: "50x50#" }, default_url: "default_item_pic.jpg"
-  validates_attachment_content_type :picture, content_type: /\Aimage\/.*\z/
+  validates_attachment_content_type :picture, content_type: %r{/\Aimage\/.*\z/}
 
   has_many :items_categories
   has_many :categories, through: :items_categories
--- app/models/items_category.rb
+++ app/models/items_category.rb
@@ -1,7 +1,7 @@
 class ItemsCategory < ApplicationRecord
-	belongs_to :item
-	belongs_to :category
+  belongs_to :item
+  belongs_to :category
 
-	validates :item_id, 		presence: true
-	validates :category_id, presence: true
+  validates :item_id, presence: true
+  validates :category_id, presence: true
 end
