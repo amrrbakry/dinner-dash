ca1ab476f27b927a3c55af8b3fe51beb5f0b58fd
Amrr Bakry <amrrbakry17@gmail.com>
2017-04-18 23:34:01 +0200

    add tests for option model

--- test/models/option_test.rb
+++ test/models/option_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
 
 class OptionTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @item = Item.first
+    @option = @item.options.build(name: "option1")
+  end
+
+  test "option is valid" do
+    assert @option.valid?
+  end
+
+  test "name is presnet" do
+    @option.name = nil
+    assert_not @option.valid?
+  end
 end
