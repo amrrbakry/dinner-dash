015fa484e54024a1395387e01b532721b606fb61
Amrr Bakry <amrrbakry17@gmail.com>
2017-04-18 23:36:56 +0200

    add validations to option model

--- app/models/option.rb
+++ app/models/option.rb
@@ -1,3 +1,6 @@
 class Option < ApplicationRecord
+  validates :name, presence: true
+  has_attached_file :picture, styles: { medium: "50x50#" }
+  validates_attachment_content_type :picture, content_type: %r{/\Aimage\/.*\z/}
   belongs_to :item
 end
