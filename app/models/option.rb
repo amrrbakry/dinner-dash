7ceb5ecbace6aa9a2ed3d2e7ced29183f9f7f537
Amrr Bakry <amrrbakry17@gmail.com>
2017-04-19 01:40:16 +0200

    fix broken option picture

--- app/models/option.rb
+++ app/models/option.rb
@@ -1,6 +1,6 @@
 class Option < ApplicationRecord
   validates :name, presence: true
-  has_attached_file :o_picture, styles: { medium: "50x50#" }
+  has_attached_file :o_picture, styles: { medium: "120x120#" }
   validates_attachment_content_type :o_picture, content_type: /\Aimage/
   validates_attachment_file_name :o_picture, matches: [/png\z/, /jpe?g\z/]
   validates_attachment_size :o_picture, less_than: 2.megabytes
