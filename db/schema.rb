2c72797a822edf78760d6cf61e96cfe3ed457e6b
Amrr Bakry <amrrbakry17@gmail.com>
2017-04-19 21:34:13 +0200

    reset db and run migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170419151346) do
+ActiveRecord::Schema.define(version: 20170419153305) do
+
   create_table "categories", force: :cascade do |t|
     t.string   "title"
     t.datetime "created_at",  null: false
