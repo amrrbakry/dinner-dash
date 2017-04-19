fe3c44347d6729ccda2319174eb2c6b035202622
Amrr Bakry <amrrbakry17@gmail.com>
2017-04-19 17:19:26 +0200

    add tests for option model

--- test/models/option_test.rb
+++ test/models/option_test.rb
@@ -10,8 +10,13 @@ class OptionTest < ActiveSupport::TestCase
     assert @option.valid?
   end
 
-  test "name is presnet" do
+  test "name is present" do
     @option.name = nil
+    assert_not @option.valid?
+  end
+
+  test "item_id is present" do
+    @option.item_id = nil
     assert_not @option.valid?
   end
 end
