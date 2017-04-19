3278b95575e77679b78988a8181b0300cce2efaa
Amrr Bakry <amrrbakry17@gmail.com>
2017-04-19 17:05:35 +0200

    run migrations for test db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170418222025) do
+ActiveRecord::Schema.define(version: 20170419145831) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "title"
@@ -73,6 +73,15 @@ ActiveRecord::Schema.define(version: 20170418222025) do
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  create_table "values", force: :cascade do |t|
+    t.string   "name"
+    t.decimal  "addtional_charge", precision: 8, scale: 2, default: "0.0", null: false
+    t.integer  "option_id"
+    t.datetime "created_at",                                               null: false
+    t.datetime "updated_at",                                               null: false
+    t.index ["option_id"], name: "index_values_on_option_id"
   end
 
 end
