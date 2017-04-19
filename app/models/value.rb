bfad033d96ea3510ad2b70de8242a24cd488ecda
Amrr Bakry <amrrbakry17@gmail.com>
2017-04-19 20:51:12 +0200

    add option values to item show page

--- app/models/value.rb
+++ app/models/value.rb
@@ -1,6 +1,6 @@
 class Value < ApplicationRecord
   validates :name, presence: true
   validates :option_id, presence: true
-  validates :additional_charge, numericality: { greater_than: 0 }
+  validates :additional_charge, numericality: { greater_than: -1 }
   belongs_to :option
 end
