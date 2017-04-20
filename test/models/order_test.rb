41a1843e187073ae42445029bc857bac402ffe34
Amrr Bakry <amrrbakry17@gmail.com>
2017-04-20 19:36:25 +0200

    add tests for order model

--- test/models/order_test.rb
+++ test/models/order_test.rb
@@ -1,7 +1,26 @@
 require 'test_helper'
 
 class OrderTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @order = Order.new(subtotal: 10.00, tax: 1, total: 11, status: "completed")
+  end
+
+  test "subtotal is present" do
+    @order.subtotal = nil
+    assert_not @order.valid?
+  end
+
+  test "total is present" do
+    @order.total = nil
+    assert_not @order.valid?
+  end
+
+  test "status is present" do
+    @order.status = nil
+  end
+
+  test "status is either pending, processed or completed" do
+    statuses = %i[pending processed completed]
+    assert_includes(statuses, @order.status)
+  end
 end
