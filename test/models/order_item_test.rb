3f47921539895c11db21a8325c713024f3435fa6
Amrr Bakry <amrrbakry17@gmail.com>
2017-05-12 17:56:18 +0200

    modify tests

--- test/models/order_item_test.rb
+++ test/models/order_item_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class OrderItemTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @order = orders(:one)
+    @item = items(:one)
+    @order_item = @order.order_items.create(item_id: @item.id, unit_price: 7.00, quantity: 1)
+  end
+
+  test "order item is valid" do
+    assert @order_item.valid?
+  end
 end
